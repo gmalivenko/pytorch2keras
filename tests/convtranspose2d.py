90b7223e34c4c1c86b171fccf20f848ff4156123
Grigory Malivenko <nerox8664@gmail.com>
2017-11-23 00:03:54 +0300

    Added PReLU activation.

---
+++ tests/convtranspose2d.py
@@ -0,0 +1,49 @@
+import keras  # work around segfault
+import sys
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+sys.path.append('../pytorch2keras')
+from converter import pytorch_to_keras
+
+
+class TestConvTranspose2d(nn.Module):
+    """Module for Dense conversion testing
+    """
+
+    def __init__(self, inp=10, out=16, kernel_size=3, bias=True):
+        super(TestConvTranspose2d, self).__init__()
+        self.conv2d = nn.ConvTranspose2d(inp, out, padding=1, stride=2, kernel_size=kernel_size, bias=bias)
+
+    def forward(self, x):
+        x = self.conv2d(x)
+        return x
+
+
+if __name__ == '__main__':
+    max_error = 0
+    for i in range(100):
+        kernel_size = np.random.randint(1, 7)
+        inp = np.random.randint(kernel_size + 1, 100)
+        out = np.random.randint(1, 100)
+
+        model = TestConvTranspose2d(inp, out, kernel_size, inp % 2)
+
+        input_np = np.random.uniform(0, 1, (1, inp, inp, inp))
+        input_var = Variable(torch.FloatTensor(input_np))
+        output = model(input_var)
+
+        k_model = pytorch_to_keras((inp, inp, inp,), output)
+
+        pytorch_output = output.data.numpy()
+        keras_output = k_model.predict(input_np)
+
+        error = np.max(pytorch_output - keras_output)
+        print(error)
+        if max_error < error:
+            max_error = error
+
+    print('Max error: {0}'.format(max_error))
