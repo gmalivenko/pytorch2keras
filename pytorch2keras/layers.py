0a2a0ff1f39160b6c1af0243f1a879fec357a252
Grigory Malivenko <nerox8664@gmail.com>
2017-11-25 20:35:30 +0300

    Added element-wise multiplication.

--- pytorch2keras/layers.py
+++ pytorch2keras/layers.py
@@ -72,47 +72,110 @@ def convert_convolution(node, node_name, input_name, output_name, layers):
         biases = None
         has_bias = False
 
-    if node.transposed:
-        W = weights_var.data.numpy().transpose(2, 3, 1, 0)
-        height, width, n_filters, channels = W.shape
+    if len(weights_var.data.numpy().shape) == 4:
+        if node.transposed:
+            W = weights_var.data.numpy().transpose(2, 3, 1, 0)
+            height, width, n_filters, channels = W.shape
+        else:
+            W = weights_var.data.numpy().transpose(2, 3, 1, 0)
+            height, width, channels, n_filters = W.shape
+        
+        assert node.output_padding == (0, 0)
+
+        if node.padding[0] != node.padding[1]:
+            raise ValueError('Unsuported padding size for convolution')
+
+        if node.dilation[0] != node.dilation[1]:
+            raise ValueError('Unsuported dilation rate for convolution')
+        dilation_rate = node.dilation[0]
+
+        padding = node.padding[0]
+        if padding > 0:
+            padding_name = output_name + '_pad'
+            padding_layer = keras.layers.ZeroPadding2D(
+                padding=node.padding,
+                name=padding_name
+            )
+            layers[padding_name] = padding_layer(layers[input_name])
+            input_name = padding_name
+
+        weights = None
+        if has_bias:
+            weights = [W, biases]
+        else:
+            weights = [W]
+
+        border_mode = 'valid'
+        # if padding == 1:
+        #     border_mode = 'same'
+
+        if node.transposed:
+            conv = keras.layers.Conv2DTranspose(
+                filters=n_filters,
+                kernel_size=(height, width),
+                strides=(node.stride[0], node.stride[1]),
+                padding=border_mode,
+                weights=weights,
+                use_bias=has_bias,
+                activation=None,
+                dilation_rate=dilation_rate,
+                name=output_name
+            )
+        else:
+            conv = keras.layers.Conv2D(
+                filters=n_filters,
+                kernel_size=(height, width),
+                strides=(node.stride[0], node.stride[1]),
+                padding=border_mode,
+                weights=weights,
+                use_bias=has_bias,
+                activation=None,
+                dilation_rate=dilation_rate,
+                name=output_name
+            )
+        layers[output_name] = conv(layers[input_name])
     else:
-        W = weights_var.data.numpy().transpose(2, 3, 1, 0)
-        height, width, channels, n_filters = W.shape
+        # raise ValueError('Conv1d(transposed) is unsupported now')
+        if node.transposed:
+            raise ValueError('Conv1d(transposed) is unsupported')
+        else:
+            W = weights_var.data.numpy().transpose(2, 1, 0)
+            print(W.shape)
+            width, channels, n_filters = W.shape
 
-    assert node.output_padding == (0, 0)
+        assert node.output_padding == (0, 0)
 
-    if node.padding[0] != node.padding[1]:
-        raise ValueError('Unsuported padding size for convolution')
+        if node.padding[0] != node.padding[1]:
+            raise ValueError('Unsuported padding size for convolution')
 
-    if node.dilation[0] != node.dilation[1]:
-        raise ValueError('Unsuported dilation rate for convolution')
-    dilation_rate = node.dilation[0]
+        if node.dilation[0] != node.dilation[1]:
+            raise ValueError('Unsuported dilation rate for convolution')
+        dilation_rate = node.dilation[0]
 
-    padding = node.padding[0]
-    if padding > 0:
-        padding_name = output_name + '_pad'
-        padding_layer = keras.layers.ZeroPadding2D(
-            padding=node.padding,
-            name=padding_name
-        )
-        layers[padding_name] = padding_layer(layers[input_name])
-        input_name = padding_name
+        padding = node.padding[0]
+        if padding > 0:
+            padding_name = output_name + '_pad'
+            padding_layer = keras.layers.ZeroPadding1D(
+                padding=padding,
+                name=padding_name
+            )
+            layers[padding_name] = padding_layer(layers[input_name])
+            input_name = padding_name
 
-    weights = None
-    if has_bias:
-        weights = [W, biases]
-    else:
-        weights = [W]
+        weights = None
+        if has_bias:
+            weights = [W, biases]
+        else:
+            weights = [W]
 
-    border_mode = 'valid'
-    # if padding == 1:
-    #     border_mode = 'same'
+        border_mode = 'valid'
+        # if padding == 1:
+        #     border_mode = 'same'
 
-    if node.transposed:
-        conv = keras.layers.Conv2DTranspose(
+        conv = keras.layers.Conv1D(
             filters=n_filters,
-            kernel_size=(height, width),
-            strides=(node.stride[0], node.stride[1]),
+            kernel_size=(width),
+            strides=(node.stride[0]),
             padding=border_mode,
             weights=weights,
             use_bias=has_bias,
@@ -120,19 +183,8 @@ def convert_convolution(node, node_name, input_name, output_name, layers):
             dilation_rate=dilation_rate,
             name=output_name
         )
-    else:
-        conv = keras.layers.Conv2D(
-            filters=n_filters,
-            kernel_size=(height, width),
-            strides=(node.stride[0], node.stride[1]),
-            padding=border_mode,
-            weights=weights,
-            use_bias=has_bias,
-            activation=None,
-            dilation_rate=dilation_rate,
-            name=output_name
-        )
-    layers[output_name] = conv(layers[input_name])
+
+        layers[output_name] = conv(layers[input_name])
 
 
 def convert_batchnorm(node, node_name, input_name, output_name, layers):
@@ -234,25 +286,6 @@ def convert_pooling(node, node_name, input_name, output_name, layers):
         raise ValueError('Unknown pooling type')
 
     layers[output_name] = pooling(layers[input_name])
-
-
-def convert_elementwise_add(node, node_name, input_names, output_name, layers):
-    """
-    Convert elementwise addition.
-
-    Args:
-        node: pytorch node element.
-        node_name: pytorch node name
-        input_name: pytorch input node name
-        output_name: pytorch output node name
-        layers: dictionary with keras tensors
-    """
-    print('Conerting elementwise_add ...')
-    model0 = layers[input_names[0]]
-    model1 = layers[input_names[1]]
-
-    add = keras.layers.Add(name=output_name)
-    layers[output_name] = add([model0, model1])
 
 
 def convert_threshold(node, node_name, input_name, output_name, layers):
@@ -419,13 +452,50 @@ def convert_dropout(node, node_name, input_name, output_name, layers):
     layers[output_name] = dropout(layers[input_name])
 
 
+def convert_elementwise_add(node, node_name, input_names, output_name, layers):
+    """
+    Convert elementwise addition.
+
+    Args:
+        node: pytorch node element.
+        node_name: pytorch node name
+        input_name: pytorch input node name
+        output_name: pytorch output node name
+        layers: dictionary with keras tensors
+    """
+    print('Conerting elementwise_add ...')
+    model0 = layers[input_names[0]]
+    model1 = layers[input_names[1]]
+
+    add = keras.layers.Add(name=output_name)
+    layers[output_name] = add([model0, model1])
+
+
+def convert_elementwise_mul(node, node_name, input_names, output_name, layers):
+    """
+    Convert elementwise multiplication.
+
+    Args:
+        node: pytorch node element.
+        node_name: pytorch node name
+        input_name: pytorch input node name
+        output_name: pytorch output node name
+        layers: dictionary with keras tensors
+    """
+    print('Conerting elementwise_mul ...')
+    model0 = layers[input_names[0]]
+    model1 = layers[input_names[1]]
+
+    mul = keras.layers.Multiply(name=output_name)
+    layers[output_name] = mul([model0, model1])
+
+
 AVAILABLE_CONVERTERS = {
     'Addmm': convert_dense,
     'ConvNd': convert_convolution,
     'BatchNorm': convert_batchnorm,
     'MaxPool2d': convert_pooling,
     'AvgPool2d': convert_pooling,
-    'Add': convert_elementwise_add,
     'View': convert_reshape,
     'Threshold': convert_threshold,
     'LeakyReLU': convert_leakyrelu,
@@ -437,4 +507,6 @@ AVAILABLE_CONVERTERS = {
     'Softsign': convert_softsign,
     'Sigmoid': convert_sigmoid,
     'Dropout': convert_dropout,
+    'Add': convert_elementwise_add,
+    'Mul': convert_elementwise_mul,
 }
---
+++ tests/mul.py
@@ -0,0 +1,51 @@
+import keras  # work around segfault
+import sys
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+sys.path.append('../pytorch2keras')
+from converter import pytorch_to_keras
+
+
+class TestMul(nn.Module):
+    """Module for Element-wise multiplication conversion testing
+    """
+
+    def __init__(self, inp=10, out=16, kernel_size=3, bias=True):
+        super(TestMul, self).__init__()
+        self.conv2d_1 = nn.Conv2d(inp, out, stride=(inp % 3 + 1), kernel_size=kernel_size, bias=bias)
+        self.conv2d_2 = nn.Conv2d(inp, out, stride=(inp % 3 + 1), kernel_size=kernel_size, bias=bias)
+
+    def forward(self, x):
+        x1 = self.conv2d_1(x)
+        x2 = self.conv2d_2(x)
+        return x1 * x2
+
+
+if __name__ == '__main__':
+    max_error = 0
+    for i in range(100):
+        kernel_size = np.random.randint(1, 7)
+        inp = np.random.randint(kernel_size + 1, 100)
+        out = np.random.randint(1, 100)
+
+        model = TestMul(inp, out, kernel_size, inp % 2)
+
+        input_np = np.random.uniform(0, 1, (1, inp, inp, inp))
+        input_var = Variable(torch.FloatTensor(input_np))
+        output = model(input_var)
+
+        k_model = pytorch_to_keras((inp, inp, inp,), output)
+
+        pytorch_output = output.data.numpy()
+        keras_output = k_model.predict(input_np)
+
+        error = np.max(pytorch_output - keras_output)
+        print(error)
+        if max_error < error:
+            max_error = error
+
+    print('Max error: {0}'.format(max_error))
