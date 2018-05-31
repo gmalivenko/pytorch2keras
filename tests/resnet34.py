78b11e8c6b28ec2039d4d42a5fc82494b005ed2e
Grigory Malivenko <nerox8664@gmail.com>
2018-05-31 23:06:38 +0300

    Added ResNet34 test case.

---
+++ tests/resnet34.py
@@ -0,0 +1,29 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
+from pytorch2keras.converter import pytorch_to_keras
+import torchvision
+
+
+if __name__ == '__main__':
+    max_error = 0
+    for i in range(10):
+        model = torchvision.models.resnet34()
+        for m in model.modules():
+            m.training = False
+
+        input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
+        input_var = Variable(torch.FloatTensor(input_np))
+        output = model(input_var)
+
+        k_model = pytorch_to_keras(model, input_var, (3, 224, 224,), verbose=True)
+
+        pytorch_output = output.data.numpy()
+        keras_output = k_model.predict(input_np)
+
+        error = np.max(pytorch_output - keras_output)
+        print(error)
+        if max_error < error:
+            max_error = error
+
+    print('Max error: {0}'.format(max_error))
