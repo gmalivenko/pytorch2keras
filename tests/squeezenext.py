f71e1d308d361afc351b3515b972e0b4ecc207f0
Grigory Malivenko <nerox8664@gmail.com>
2018-08-17 22:46:08 +0300

    Added SqueezeNext model test.

---
+++ tests/squeezenext.py
@@ -0,0 +1,407 @@
+"""
+Model from https://github.com/osmr/imgclsmob/tree/master/pytorch/models
+"""
+
+
+import numpy as np
+import torch
+from torch.autograd import Variable
+from pytorch2keras.converter import pytorch_to_keras
+import torchvision
+
+import os
+import torch.nn as nn
+import torch.nn.init as init
+
+
+class SqnxtConv(nn.Module):
+    """
+    SqueezeNext specific convolution block.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    kernel_size : int or tuple/list of 2 int
+        Convolution window size.
+    stride : int or tuple/list of 2 int
+        Strides of the convolution.
+    padding : int or tuple/list of 2 int, default (0, 0)
+        Padding value for convolution layer.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride,
+                 padding=(0, 0)):
+        super(SqnxtConv, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.activ = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activ(x)
+        return x
+
+
+class SqnxtUnit(nn.Module):
+    """
+    SqueezeNext unit.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int
+        Strides of the convolution.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride):
+        super(SqnxtUnit, self).__init__()
+        if stride == 2:
+            reduction_den = 1
+            self.resize_identity = True
+        elif in_channels > out_channels:
+            reduction_den = 4
+            self.resize_identity = True
+        else:
+            reduction_den = 2
+            self.resize_identity = False
+
+        self.conv1 = SqnxtConv(
+            in_channels=in_channels,
+            out_channels=(in_channels // reduction_den),
+            kernel_size=1,
+            stride=stride)
+        self.conv2 = SqnxtConv(
+            in_channels=(in_channels // reduction_den),
+            out_channels=(in_channels // (2 * reduction_den)),
+            kernel_size=1,
+            stride=1)
+        self.conv3 = SqnxtConv(
+            in_channels=(in_channels // (2 * reduction_den)),
+            out_channels=(in_channels // reduction_den),
+            kernel_size=(1, 3),
+            stride=1,
+            padding=(0, 1))
+        self.conv4 = SqnxtConv(
+            in_channels=(in_channels // reduction_den),
+            out_channels=(in_channels // reduction_den),
+            kernel_size=(3, 1),
+            stride=1,
+            padding=(1, 0))
+        self.conv5 = SqnxtConv(
+            in_channels=(in_channels // reduction_den),
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1)
+
+        if self.resize_identity:
+            self.identity_conv = SqnxtConv(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=1,
+                stride=stride)
+        self.activ = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        if self.resize_identity:
+            identity = self.identity_conv(x)
+        else:
+            identity = x
+        identity = self.activ(identity)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = x + identity
+        x = self.activ(x)
+        return x
+
+
+class SqnxtInitBlock(nn.Module):
+    """
+    SqueezeNext specific initial block.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels):
+        super(SqnxtInitBlock, self).__init__()
+        self.conv = SqnxtConv(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=7,
+            stride=2,
+            padding=1)
+        self.pool = nn.MaxPool2d(
+            kernel_size=3,
+            stride=2,
+            ceil_mode=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.pool(x)
+        return x
+
+
+class SqueezeNext(nn.Module):
+    """
+    SqueezeNext model from 'SqueezeNext: Hardware-Aware Neural Network Design,' https://arxiv.org/abs/1803.10615.
+
+    Parameters:
+    ----------
+    channels : list of list of int
+        Number of output channels for each unit.
+    init_block_channels : int
+        Number of output channels for the initial unit.
+    final_block_channels : int
+        Number of output channels for the final block of the feature extractor.
+    in_channels : int, default 3
+        Number of input channels.
+    num_classes : int, default 1000
+        Number of classification classes.
+    """
+    def __init__(self,
+                 channels,
+                 init_block_channels,
+                 final_block_channels,
+                 in_channels=3,
+                 num_classes=1000):
+        super(SqueezeNext, self).__init__()
+
+        self.features = nn.Sequential()
+        self.features.add_module("init_block", SqnxtInitBlock(
+            in_channels=in_channels,
+            out_channels=init_block_channels))
+        in_channels = init_block_channels
+        for i, channels_per_stage in enumerate(channels):
+            stage = nn.Sequential()
+            for j, out_channels in enumerate(channels_per_stage):
+                stride = 2 if (j == 0) and (i != 0) else 1
+                stage.add_module("unit{}".format(j + 1), SqnxtUnit(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    stride=stride))
+                in_channels = out_channels
+            self.features.add_module("stage{}".format(i + 1), stage)
+        self.features.add_module('final_block', SqnxtConv(
+            in_channels=in_channels,
+            out_channels=final_block_channels,
+            kernel_size=1,
+            stride=1))
+        in_channels = final_block_channels
+        self.features.add_module('final_pool', nn.AvgPool2d(
+            kernel_size=7,
+            stride=1))
+
+        self.output = nn.Linear(
+            in_features=in_channels,
+            out_features=num_classes)
+
+        self._init_params()
+
+    def _init_params(self):
+        for name, module in self.named_modules():
+            if isinstance(module, nn.Conv2d):
+                init.kaiming_uniform_(module.weight)
+                if module.bias is not None:
+                    init.constant_(module.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.output(x)
+        return x
+
+
+def get_squeezenext(version,
+                    width_scale,
+                    model_name=None,
+                    pretrained=False,
+                    root=os.path.join('~', '.torch', 'models'),
+                    **kwargs):
+    """
+    Create SqueezeNext model with specific parameters.
+
+    Parameters:
+    ----------
+    version : str
+        Version of SqueezeNet ('23' or '23v5').
+    width_scale : float
+        Scale factor for width of layers.
+    model_name : str or None, default None
+        Model name for loading pretrained model.
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+
+    init_block_channels = 64
+    final_block_channels = 128
+    channels_per_layers = [32, 64, 128, 256]
+
+    if version == '23':
+        layers = [6, 6, 8, 1]
+    elif version == '23v5':
+        layers = [2, 4, 14, 1]
+    else:
+        raise ValueError("Unsupported SqueezeNet version {}".format(version))
+
+    channels = [[ci] * li for (ci, li) in zip(channels_per_layers, layers)]
+
+    if width_scale != 1:
+        channels = [[int(cij * width_scale) for cij in ci] for ci in channels]
+        init_block_channels = int(init_block_channels * width_scale)
+        final_block_channels = int(final_block_channels * width_scale)
+
+    net = SqueezeNext(
+        channels=channels,
+        init_block_channels=init_block_channels,
+        final_block_channels=final_block_channels,
+        **kwargs)
+
+    if pretrained:
+        if (model_name is None) or (not model_name):
+            raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
+        import torch
+        from .model_store import get_model_file
+        net.load_state_dict(torch.load(get_model_file(
+            model_name=model_name,
+            local_model_store_dir_path=root)))
+
+    return net
+
+
+def sqnxt23_w1(**kwargs):
+    """
+    1.0-SqNxt-23 model from 'SqueezeNext: Hardware-Aware Neural Network Design,' https://arxiv.org/abs/1803.10615.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenext(version="23", width_scale=1.0, model_name="sqnxt23_w1", **kwargs)
+
+
+def sqnxt23_w3d2(**kwargs):
+    """
+    0.75-SqNxt-23 model from 'SqueezeNext: Hardware-Aware Neural Network Design,' https://arxiv.org/abs/1803.10615.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenext(version="23", width_scale=1.5, model_name="sqnxt23_w3d2", **kwargs)
+
+
+def sqnxt23_w2(**kwargs):
+    """
+    0.5-SqNxt-23 model from 'SqueezeNext: Hardware-Aware Neural Network Design,' https://arxiv.org/abs/1803.10615.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenext(version="23", width_scale=2.0, model_name="sqnxt23_w2", **kwargs)
+
+
+def sqnxt23v5_w1(**kwargs):
+    """
+    1.0-SqNxt-23v5 model from 'SqueezeNext: Hardware-Aware Neural Network Design,' https://arxiv.org/abs/1803.10615.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenext(version="23v5", width_scale=1.0, model_name="sqnxt23v5_w1", **kwargs)
+
+
+def sqnxt23v5_w3d2(**kwargs):
+    """
+    0.75-SqNxt-23v5 model from 'SqueezeNext: Hardware-Aware Neural Network Design,' https://arxiv.org/abs/1803.10615.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenext(version="23v5", width_scale=1.5, model_name="sqnxt23v5_w3d2", **kwargs)
+
+
+def sqnxt23v5_w2(**kwargs):
+    """
+    0.5-SqNxt-23v5 model from 'SqueezeNext: Hardware-Aware Neural Network Design,' https://arxiv.org/abs/1803.10615.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenext(version="23v5", width_scale=2.0, model_name="sqnxt23v5_w2", **kwargs)
+
+
+
+
+if __name__ == '__main__':
+    max_error = 0
+    for i in range(10):
+        model = sqnxt23_w1()
+        for m in model.modules():
+            m.training = False
+
+        input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
+        input_var = Variable(torch.FloatTensor(input_np))
+        output = model(input_var)
+
+        k_model = pytorch_to_keras(model, input_var, (3, 224, 224,), verbose=True)
+
+        pytorch_output = output.data.numpy()
+        keras_output = k_model.predict(input_np)
+
+        error = np.max(pytorch_output - keras_output)
+        print(error)
+        if max_error < error:
+            max_error = error
+
+    print('Max error: {0}'.format(max_error))
