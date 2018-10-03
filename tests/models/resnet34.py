61619cf38303f97f29d43a35bbcbad73e9a42790
Grigory Malivenko <nerox8664@gmail.com>
2018-10-03 14:03:08 +0300

    Update some tests.

--- tests/models/resnet34.py
+++ tests/models/resnet34.py
@@ -1,16 +1,27 @@
 import numpy as np
 import torch
+import torch.nn as nn
 from torch.autograd import Variable
 from pytorch2keras.converter import pytorch_to_keras
 import torchvision
 
 
+def check_error(output, k_model, input_np, epsilon=1e-5):
+    pytorch_output = output.data.numpy()
+    keras_output = k_model.predict(input_np)
+
+    error = np.max(pytorch_output - keras_output)
+    print('Error:', error)
+
+    assert error < epsilon
+    return error
+
+
 if __name__ == '__main__':
     max_error = 0
-    for i in range(10):
+    for i in range(100):
         model = torchvision.models.resnet34()
-        for m in model.modules():
-            m.training = False
+        model.eval()
 
         input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
         input_var = Variable(torch.FloatTensor(input_np))
@@ -18,11 +29,7 @@ if __name__ == '__main__':
 
         k_model = pytorch_to_keras(model, input_var, (3, 224, 224,), verbose=True)
 
-        pytorch_output = output.data.numpy()
-        keras_output = k_model.predict(input_np)
-
-        error = np.max(pytorch_output - keras_output)
-        print(error)
+        error = check_error(output, k_model, input_np)        
         if max_error < error:
             max_error = error
 
