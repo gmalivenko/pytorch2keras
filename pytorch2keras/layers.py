898e37c0b62ef9458922514080c24696596ce66f
Grigory Malivenko <nerox8664@gmail.com>
2018-11-24 19:15:46 +0300

    Reimplement clip layer.

--- pytorch2keras/layers.py
+++ pytorch2keras/layers.py
@@ -1444,6 +1444,28 @@ def convert_shape(params, w_name, scope_name, inputs, layers, weights, names):
     layers[scope_name] = lambda_layer(layers[inputs[0]])
 
 
+def convert_clip(params, w_name, scope_name, inputs, layers, weights, names):
+    """
+    Convert clip operation.
+
+    Args:
+        params: dictionary with layer parameters
+        w_name: name prefix in state_dict
+        scope_name: pytorch scope name
+        inputs: pytorch node inputs
+        layers: dictionary with keras tensors
+        weights: pytorch state_dict
+        names: use short names for keras layers
+    """
+    print('Converting clip ...')
+
+    def target_layer(x, vmin=params['min'], vmax=params['max']):
+        return tf.clip_by_value(x, vmin, vmax)
+
+    lambda_layer = keras.layers.Lambda(target_layer)
+    layers[scope_name] = lambda_layer(layers[inputs[0]])
+
+
 AVAILABLE_CONVERTERS = {
     'onnx::Conv': convert_conv,
     'onnx::ConvTranspose': convert_convtranspose,
@@ -1485,4 +1507,5 @@ AVAILABLE_CONVERTERS = {
     'onnx::Squeeze': convert_squeeze,
     'onnx::Unsqueeze': convert_unsqueeze,
     'onnx::Shape': convert_shape,
+    'onnx::Clip': convert_clip,
 }
