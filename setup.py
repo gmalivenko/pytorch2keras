a6b91b5e4409d816398d1978a77a823c5030a789
Grigory Malivenko <nerox8664@gmail.com>
2018-11-29 00:02:08 +0300

    Add Manifest.in. Fixed package.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md') as f:
 
 
 setup(name='pytorch2keras',
-      version='0.1.7',
+      version='0.1.8',
       description='The deep learning models convertor',
       long_description=long_description,
       long_description_content_type='text/markdown',
