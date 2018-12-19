04cbad42d8f177c6cf541a4048f66b0d30cbd59f
Grigory Malivenko <nerox8664@gmail.com>
2018-12-19 16:52:24 +0300

    Update average pooling test.

--- tests/layers/avg_pool.py
+++ tests/layers/avg_pool.py
@@ -9,13 +9,11 @@ class AvgPool(nn.Module):
     """Module for AveragePool conversion testing
     """
 
-    def __init__(self, inp=10, out=16, kernel_size=3, bias=True):
+    def __init__(self, stride=3, padding=0, kernel_size=3):
         super(AvgPool, self).__init__()
-        self.conv2d = nn.Conv2d(inp, out, kernel_size=kernel_size, padding=1, bias=bias)
-        self.pool = nn.AvgPool2d(kernel_size=3, padding=1, count_include_pad=False, stride=2)
+        self.pool = nn.AvgPool2d(kernel_size=kernel_size, padding=padding, stride=stride)
 
     def forward(self, x):
-        x = self.conv2d(x)
         x = self.pool(x)
         return x
 
@@ -23,17 +21,18 @@ class AvgPool(nn.Module):
 if __name__ == '__main__':
     max_error = 0
     for i in range(100):
-        kernel_size = np.random.randint(4, 7)
+        kernel_size = np.random.randint(2, 7)
+        stride = np.random.randint(1, kernel_size)
+        padding = np.random.randint(1, kernel_size/2 + 1)
         inp = np.random.randint(kernel_size + 1, 100)
-        out = np.random.randint(1, 100)
 
-        model = AvgPool(inp, out, kernel_size, inp % 2)
+        model = AvgPool(kernel_size=kernel_size, padding=padding, stride=stride)
 
         input_np = np.random.uniform(0, 1, (1, inp, inp, inp))
         input_var = Variable(torch.FloatTensor(input_np))
         output = model(input_var)
 
-        k_model = pytorch_to_keras(model, input_var, (inp, inp, inp,), verbose=True, names='keep')
+        k_model = pytorch_to_keras(model, input_var, (inp, inp, inp,), verbose=False, names='keep')
         print(k_model.summary())
         pytorch_output = output.data.numpy()
         keras_output = k_model.predict(input_np)
