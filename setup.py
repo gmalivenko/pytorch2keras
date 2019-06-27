52c4160066a018bd9d9499cc4bc2220bf3e042c8
Grigory Malivenko <nerox8664@gmail.com>
2019-06-27 12:01:13 +0300

    Update pypi version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md') as f:
 
 
 setup(name='pytorch2keras',
-      version='0.2.0',
+      version='0.2.1',
       description='The deep learning models convertor',
       long_description=long_description,
       long_description_content_type='text/markdown',
